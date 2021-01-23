343068aae90e42634472b8634ba45531b3322fab
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-01-23 11:09:12 -0600

    added callback functions

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('CONNECTED');
+// CALLBACKS
+const acceptsString1 = (str) => {
+	return str;
+};
+
+const acceptsString2 = (str) => {
+	return str;
+};
+
+const finishedString = (str, func) => {
+	return func(str);
+};
+
+console.log(finishedString());
